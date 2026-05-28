19053cc9304f21f741a47f83d8208e23fff33d74
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 15:59:35 -0600

    Update azurerm-eventhub-namespace/outputs.tf

--- modules/azurerm-eventhub-namespace/outputs.tf
+++ modules/azurerm-eventhub-namespace/outputs.tf
@@ -1,4 +1,7 @@
 output "this" {
   description = "Azure Event Hub Namespace"
-  value       = azurerm_eventhub_namespace.this
+  value = { 
+    id   = azurerm_eventhub_namespace.this.id 
+    name = azurerm_eventhub_namespace.this.name 
+  }
 }
