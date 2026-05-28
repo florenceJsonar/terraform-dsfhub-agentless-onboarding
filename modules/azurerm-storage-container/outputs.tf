28dd57fce4897230e680dd22ffb06b1ea4fa4f7e
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 16:03:49 -0600

    Update azurerm-storage-container/outputs.tf

--- modules/azurerm-storage-container/outputs.tf
+++ modules/azurerm-storage-container/outputs.tf
@@ -1,4 +1,7 @@
 output "this" {
   description = "Azure storage container"
-  value       = azurerm_storage_container.this
+  value = {   
+    id   = azurerm_storage_container.this.id     
+    name = azurerm_storage_container.this.name    
+  }
 }
