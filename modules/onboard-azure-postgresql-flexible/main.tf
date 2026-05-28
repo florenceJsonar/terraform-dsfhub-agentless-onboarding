8379fd81b19eb5df5b58b7416c0f145ba184ae52
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 16:09:47 -0600

    Update onboard-azure-postgresql-flexible/main.tf

--- modules/onboard-azure-postgresql-flexible/main.tf
+++ modules/onboard-azure-postgresql-flexible/main.tf
@@ -48,7 +48,7 @@ module "postgresql-diagnostic-setting" {
   enabled_log                    = [{ category = "PostgreSQLLogs" }]
   eventhub_authorization_rule_id = var.diagnostic_setting_eventhub_authorization_rule_id
   eventhub_name                  = var.diagnostic_setting_eventhub_name
-  metric                         = null
+  enabled_metric                 = null
   name                           = var.diagnostic_setting_name
   storage_account_id             = null
   target_resource_id             = module.azure-postgresql-flexible-server.this.id
