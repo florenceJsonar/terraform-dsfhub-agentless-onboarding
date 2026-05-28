e222de294de14b6d4483b9ce93aa3cafda20a25a
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 15:58:24 -0600

    Update azurerm-monitor-diagnostic-setting/outputs.tf

--- modules/azurerm-monitor-diagnostic-setting/outputs.tf
+++ modules/azurerm-monitor-diagnostic-setting/outputs.tf
@@ -1,4 +1,7 @@
 output "this" {
   description = "Diagnostic Setting"
-  value       = azurerm_monitor_diagnostic_setting.this
+  value       = {
+    id   = azurerm_monitor_diagnostic_setting.this.id
+    name = azurerm_monitor_diagnostic_setting.this.name 
+  }
 }
