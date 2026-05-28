ccdd2354dacfa14d3c0e67b6f311f67920eab086
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 16:00:38 -0600

    Update azurerm-mssql-server/outputs.tf

--- modules/azurerm-mssql-server/outputs.tf
+++ modules/azurerm-mssql-server/outputs.tf
@@ -1,4 +1,8 @@
 output "this" {
   description = "Microsoft SQL Azure Database Server"
-  value       = azurerm_mssql_server.this
+  value = {  
+    id                          = azurerm_mssql_server.this.id
+    name                        = azurerm_mssql_server.this.name 
+    fully_qualified_domain_name = azurerm_mssql_server.this.fully_qualified_domain_name  
+  }
 }
