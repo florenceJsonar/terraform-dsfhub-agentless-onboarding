559edc97c0e4a3df5707e01998b9a2d6f830640a
Florence Fernandes <florence.fernandes@thalesgroup.com>
2026-05-28 16:03:00 -0600

    Update azurerm-storage-account/outputs.tf

--- modules/azurerm-storage-account/outputs.tf
+++ modules/azurerm-storage-account/outputs.tf
@@ -1,4 +1,8 @@
 output "this" {
   description = "Azure storage account"
-  value       = azurerm_storage_account.this
+  value = {  
+    id                 = azurerm_storage_account.this.id  
+    name               = azurerm_storage_account.this.name
+    primary_access_key = azurerm_storage_account.this.primary_access_key 
+  }
 }
